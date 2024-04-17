006e6769fd34e5e363a312c3b722d413294dd930
Ng Heng Khai <nhkhai@gmail.com>
2024-04-17 21:33:43 +0800

    Fixed the test case.

--- controller.js
+++ controller.js
@@ -1,6 +1,6 @@
 // This is the only function that will be called by the endpoint.
 function print(req, res) {
-  res.send("Hello I am Heng Khai! ");
+  res.send("Hello I'am Heng Khai!");
 }
 
 function print_home(req, res) {

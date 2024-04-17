7b702b0f173fcdd638695cca1a84a372d2549a0b
Ng Heng Khai <nhkhai@gmail.com>
2024-04-17 21:29:55 +0800

    Updated with my answers.

--- controller.js
+++ controller.js
@@ -1,6 +1,10 @@
 // This is the only function that will be called by the endpoint.
 function print(req, res) {
-  res.send("Hello World!!");
+  res.send("Hello I am Heng Khai! ");
+}
+
+function print_home(req, res) {
+  res.send("This is the home page");
 }
 
 module.exports = print;
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const print = require("./controller");
 
 app.get("/", print);
 app.get("/hello", print);
+app.get("/home", print_home);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening to port ${process.env.PORT}`);
